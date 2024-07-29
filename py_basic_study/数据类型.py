5f563bc82d70bd941cf9493c753d9579c1514263
rootLong <2753938295@qq.com>
2024-07-29 20:45:15 +0800

    数据类型学习

---
+++ py_basic_study/数据类型.py
@@ -0,0 +1,21 @@
+a = 60
+print(type(a))
+a = 13.14
+print(type(a))
+a = "\"Hello"
+print(type(a))
+a = type(a)
+print(a)
+
+# 数字类型转换成字符串
+num_str = str(11)
+print(type(num_str), num_str)
+float_str = str(3.14)
+print(type(float_str), float_str)
+# 字符串类型转换成数字类型
+num = int("11")
+print(type(num), num)
+num2 = float(3)
+print(type(num2), num2)
+num3 = int(3.14)
+print(type(num3), num3)
