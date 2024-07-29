766ee9434b2d8fb753c996dfede3efa575b2803f
rootLong <2753938295@qq.com>
2024-07-29 20:45:26 +0800

    运算符学习

---
+++ py_basic_study/运算符.py
@@ -0,0 +1,25 @@
+# 算术运算符
+print("1+1=", 1+1)
+print("2-2=", 2-2)
+print("2*2=", 2*2)
+print("3/4=", 3/4)
+print("11//5=", 11//5)
+print("3%4=", 3 % 4)
+print("4**4=", 4**4)
+
+num = 1
+num += 1
+print(num)
+num -= 1
+print(num)
+num *= 2
+print(num)
+num /= 2
+print(num)
+num %= 3
+print(num)
+num **= 4
+print(num)
+num //= 2
+print(num)
+
