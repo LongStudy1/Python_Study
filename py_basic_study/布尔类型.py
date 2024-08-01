eecc034a14406adde4bb60d5d866860dc2d5125f
rootLong <2753938295@qq.com>
2024-08-01 21:52:33 +0800

    布尔类型学习

---
+++ py_basic_study/布尔类型.py
@@ -0,0 +1,12 @@
+"""
+-Python中True：1，False：0
+"""
+bool_1 = True
+bool_2 = False
+print(f"bool_1 is {bool_1}，类型是：{type(bool_1)} and bool_2 is {bool_2},类型是：{type(bool_1)}")
+result = 10 > 5
+print(f"10 > 5的结果是：{result}")
+num1 = 10
+num2 = 10
+print(f"{num1} = {num2}的结果是：{num1==num2}")
+print(f"{num1} > {num2}的结果是：{num1>num2}")
