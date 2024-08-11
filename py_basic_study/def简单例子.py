5c3de89fa8e31e5c1d7c8487d16017af7a006027
rootLong <2753938295@qq.com>
2024-08-11 18:10:46 +0800

    def简单例子

---
+++ py_basic_study/def简单例子.py
@@ -0,0 +1,14 @@
+def check():
+    print("欢迎来到佛山\n请出示你的健康码")
+
+
+# check()
+
+
+def add(a, b, z):
+    result = a + b + z
+    print(f"{a}+{b}+{z}的结果是：{result}")
+    return a + b
+
+
+add(1, 8568, 85)
