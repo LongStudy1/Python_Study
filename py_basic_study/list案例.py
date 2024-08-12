79cd59c0c1ee201334d6c0cf65f981e7fdb4c073
rootLong <2753938295@qq.com>
2024-08-12 21:52:38 +0800

    list案例学习

---
+++ py_basic_study/list案例.py
@@ -0,0 +1,9 @@
+mylist = [21, 25, 21, 23, 22, 20, 31]
+mylist.extend([29, 33, 30])
+num1 = mylist[0]
+print(num1)
+num2 = mylist[-1]
+print(num2)
+num3 = mylist.index(31)
+print(num3)
+print(mylist)
